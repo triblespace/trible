b38fb5d6bfb432e6caca0c1818cd81fc41f00d98
⚫️ <jp@bultmann.eu>
2024-05-23 11:13:24 +0200

    add chat subcommand based on candle examples

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,40 @@
-use anyhow::Result;
-use structopt::StructOpt;
-use rand::{rngs::OsRng, RngCore};
+mod chat;
 
-#[derive(StructOpt)]
+use clap::{Parser, Subcommand};
+use anyhow::Result;
+use rand::{rngs::OsRng, RngCore};
+use chat::{chat, ChatArgs};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
 /// A knowledge graph and meta file system for object stores.
 ///
-enum TribleCli {
+struct TribleCli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
     /// Generate a new random id.
-    IdGen {},
+    IdGen{},
+    /// Chat with your knowledge base.
+    Chat(ChatArgs),
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args = TribleCli::from_args();
-    match args {
-        TribleCli::IdGen {} => {
+    let args = TribleCli::parse();
+    match args.command {
+        Commands::IdGen{} => {
             let mut id = [0u8; 16];
             OsRng.fill_bytes(&mut id);
             let encoded_id = hex::encode(id);
             println!("{}", encoded_id.to_ascii_uppercase());
         }
+        Commands::Chat(args) => {
+            chat(args).unwrap();
+        },
     }
     Ok(())
 }
